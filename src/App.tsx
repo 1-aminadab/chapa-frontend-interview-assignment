bf3b9065b3389c6cd7c962aac2b972f37b724b2c
1-aminadab <developer.amanuel@gmail.com>
2025-07-08 14:56:49 +0300

    tailwind css setup - as the current version of is v4 the setup is a little bit different that v3 https://tailwindcss.com/docs/installation/using-vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './index.css' // <- Make sure Tailwind directives are in this file
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
